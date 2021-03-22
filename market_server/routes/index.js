0640d6555958da663420a95e631a7219f3ac6a42
oh980225 <oh980225@gmail.com>
2021-03-22 10:42:16 +0900

    First COMMIT

---
+++ market_server/routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+const member = [{
+    "number": "17101223",
+    "name": "오승재",
+    "part": "BACK"
+  },
+  {
+    "number": "17101212",
+    "name": "박지수",
+    "part": "BACK"
+  },
+  {
+    "number": "17101245",
+    "name": "조인혁",
+    "part": "FRONT"
+  },
+  {
+    "number": "17101220",
+    "name": "신성일",
+    "part": "FRONT"
+  }
+];
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: '6 TEAM',
+    member
+  });
+});
+
+module.exports = router;
