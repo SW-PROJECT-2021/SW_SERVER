0b6a8b030f3be59129500c3bc8ade398673d04be
oh980225 <oh980225@gmail.com>
2021-05-31 18:39:24 +0900

    [EDIT] 인기상품 관련

--- market_server/routes/sold/index.js
+++ market_server/routes/sold/index.js
@@ -5,9 +5,7 @@ const auth = require('../../middlewares/auth');
 
 router.get('/', auth.checkSession, auth.checkAdmin, soldController.getTotalIncome); 
 router.get('/search', auth.checkSession, auth.checkAdmin, soldController.getTotalIncomeByDate); 
-router.get('/rank/product', auth.checkSession, auth.checkAdmin, soldController.getTopProducts); 
+router.get('/rank/product', auth.checkSession, soldController.getTopProducts); 
 router.get('/rank/category', auth.checkSession, auth.checkAdmin, soldController.getTopCategory); 
-
-// router.get('/:id', auth.checkSession, auth.checkAdmin, soldController.);
 
 module.exports = router;
